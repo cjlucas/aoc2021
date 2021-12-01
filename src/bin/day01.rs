6fbe8f17cd8d99d67acdda1a1b6489a0e55f4dd6
Chris Lucas <chris@cjl.io>
2021-12-01 17:33:38 -0500

    use include_str! instead of std::fs

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,7 +1,9 @@
 use aoc2021::prelude::*;
 
-fn count_incrementing_windows(window_size: usize) -> usize {
-    let measurements = read_lines_as::<i64>("inputs/day01.txt");
+const INPUT: &'static str = include_str!("../../inputs/day01.txt");
+
+fn count_incrementing_windows(input: &str, window_size: usize) -> usize {
+    let measurements = parse_lines_as::<i64>(input);
 
     measurements
         .windows(window_size + 1)
@@ -9,30 +11,52 @@ fn count_incrementing_windows(window_size: usize) -> usize {
         .count()
 }
 
-fn part1() -> usize {
-    count_incrementing_windows(1)
+fn part1(input: &str) -> usize {
+    count_incrementing_windows(input, 1)
 }
 
-fn part2() -> usize {
-    count_incrementing_windows(3)
+fn part2(input: &str) -> usize {
+    count_incrementing_windows(input, 3)
 }
 
 fn main() {
-    dbg!(part1());
-    dbg!(part2());
+    dbg!(part1(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const SAMPLE_INPUT: &'static str = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+";
+
+    #[test]
+    fn test_part1_sample() {
+        assert_eq!(part1(SAMPLE_INPUT), 7);
+    }
+
     #[test]
     fn test_part1() {
-        assert_eq!(1602, part1());
+        assert_eq!(1602, part1(INPUT));
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 5);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(1633, part2());
+        assert_eq!(1633, part2(INPUT));
     }
 }
