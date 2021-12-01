86d8a665e1a3ea6ef86f654d6f18dae345b114c0
Chris Lucas <chris@cjl.io>
2021-12-01 17:36:17 -0500

    test cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -47,7 +47,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(1602, part1(INPUT));
+        assert_eq!(part1(INPUT), 1602);
     }
 
     #[test]
@@ -57,6 +57,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(1633, part2(INPUT));
+        assert_eq!(part2(INPUT), 1633);
     }
 }
