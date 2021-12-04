5b787b362710a1e14237baffe4bf2d667527271b
Chris Lucas <chris@cjl.io>
2021-12-04 00:30:56 -0500

    part2 done

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -34,11 +34,83 @@ fn part1(input: &str) -> usize {
     gamma_rate * epsilon_rate
 }
 
-// fn part2(input: &str) -> usize {}
+fn find_oxygen_generator_rating(bitstrings: Vec<String>, index: usize) -> String {
+    if bitstrings.len() == 1 {
+        return bitstrings[0].clone();
+    }
+
+    let mut cnt = 0i32;
+
+    for bs in &bitstrings {
+        if bs.chars().nth(index).unwrap() == '1' {
+            cnt += 1;
+        } else {
+            cnt -= 1;
+        }
+    }
+
+    let c = if cnt >= 0 { '1' } else { '0' };
+
+    let bitstrings = bitstrings
+        .into_iter()
+        .filter(|s| s.chars().nth(index).unwrap() == c)
+        .collect();
+
+    find_oxygen_generator_rating(bitstrings, index + 1)
+}
+
+fn find_co2_scubber_rating(bitstrings: Vec<String>, index: usize) -> String {
+    if bitstrings.len() == 1 {
+        return bitstrings[0].clone();
+    }
+
+    let mut cnt = 0i32;
+
+    for bs in &bitstrings {
+        if bs.chars().nth(index).unwrap() == '1' {
+            cnt += 1;
+        } else {
+            cnt -= 1;
+        }
+    }
+
+    let c = if cnt >= 0 { '0' } else { '1' };
+
+    let bitstrings = bitstrings
+        .into_iter()
+        .filter(|s| s.chars().nth(index).unwrap() == c)
+        .collect();
+
+    find_co2_scubber_rating(bitstrings, index + 1)
+}
+
+fn part2(input: &str) -> usize {
+    let s = find_oxygen_generator_rating(input.lines().map(str::to_string).collect(), 0);
+
+    let mut oxygen_generator_rating = 0;
+
+    for (idx, c) in s.char_indices() {
+        if c == '1' {
+            oxygen_generator_rating += 1 << (s.len() - idx - 1);
+        }
+    }
+
+    let s = find_co2_scubber_rating(input.lines().map(str::to_string).collect(), 0);
+
+    let mut co2_scrubber_rating = 0;
+
+    for (idx, c) in s.char_indices() {
+        if c == '1' {
+            co2_scrubber_rating += 1 << (s.len() - idx - 1);
+        }
+    }
+
+    oxygen_generator_rating * co2_scrubber_rating
+}
 
 fn main() {
     dbg!(part1(INPUT));
-    // dbg!(part2(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -68,13 +140,13 @@ mod tests {
         assert_eq!(part1(INPUT), 3847100);
     }
 
-    // #[test]
-    // fn test_part2_sample() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 900);
-    // }
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 230);
+    }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(INPUT), 1781819478);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 4105235);
+    }
 }
