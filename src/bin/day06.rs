5f9d0f7466d3123a8a6725bd3f4150f5117cf67b
Chris Lucas <chris@cjl.io>
2021-12-06 00:30:42 -0500

    part2 done (ez)

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -2,39 +2,39 @@ use aoc2021::prelude::*;
 
 const INPUT: &'static str = include_str!("../../inputs/day06.txt");
 
-fn part1(input: &str) -> usize {
-    let mut timers: Vec<usize> = input
-        .trim()
-        .split(',')
-        .map(|s| s.parse().unwrap())
-        .collect();
+fn simulate_laternfish_lifecycle(input: &str, num_days: usize) -> usize {
+    let mut timers = [0usize; 9];
 
-    for _ in 0..80 {
-        let mut new_fish = 0;
-        for t in &mut timers {
-            if *t == 0 {
-                new_fish += 1;
-                *t = 6;
-            } else {
-                *t -= 1;
-            }
-        }
-
-        for _ in 0..new_fish {
-            timers.push(8);
-        }
+    for timer in input.trim().split(',') {
+        let timer: usize = timer.parse().unwrap();
+        timers[timer] += 1;
     }
 
-    timers.len()
+    for _ in 0..num_days {
+        let new_fish = timers[0];
+
+        for i in 1..9 {
+            timers[i - 1] = timers[i];
+        }
+
+        timers[6] += new_fish;
+        timers[8] = new_fish;
+    }
+
+    timers.iter().sum()
+}
+
+fn part1(input: &str) -> usize {
+    simulate_laternfish_lifecycle(input, 80)
 }
 
 fn part2(input: &str) -> usize {
-    todo!()
+    simulate_laternfish_lifecycle(input, 256)
 }
 
 fn main() {
     dbg!(part1(INPUT));
-    // dbg!(part2(INPUT));
+    dbg!(part2(INPUT));
 }
 
 #[cfg(test)]
@@ -53,13 +53,13 @@ mod tests {
         assert_eq!(part1(INPUT), 353274);
     }
 
-    // #[test]
-    // fn test_part2_sample() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 12);
-    // }
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 26984457539);
+    }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(INPUT), 20373);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 1609314870967);
+    }
 }
