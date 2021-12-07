e88ff3323890731920d203b975e7160b3f5543e6
Chris Lucas <chris@cjl.io>
2021-12-06 22:04:20 -1000

    Day 7 (#8)

---
+++ src/bin/day07.rs
@@ -0,0 +1,83 @@
+#![feature(int_abs_diff)]
+
+const INPUT: &'static str = include_str!("../../inputs/day07.txt");
+
+fn calculate_fuel_usage(
+    crabs: &[usize],
+    pos: usize,
+    fuel_usage_calculator: fn(usize) -> usize,
+) -> usize {
+    crabs
+        .iter()
+        .map(|crab| fuel_usage_calculator(pos.abs_diff(*crab)))
+        .sum()
+}
+
+fn find_optimal_fuel_usage(
+    crabs: &[usize],
+    positions: &[usize],
+    fuel_usage_calculator: fn(usize) -> usize,
+) -> usize {
+    if positions.len() == 1 {
+        return calculate_fuel_usage(crabs, positions[0], fuel_usage_calculator);
+    }
+
+    let partition = positions.len() / 2;
+    let left = find_optimal_fuel_usage(crabs, &positions[..partition], fuel_usage_calculator);
+    let right = find_optimal_fuel_usage(crabs, &positions[partition..], fuel_usage_calculator);
+
+    left.min(right)
+}
+
+fn run(input: &str, fuel_usage_calculator: fn(usize) -> usize) -> usize {
+    let crab_positions: Vec<usize> = input
+        .trim()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let min_pos = *crab_positions.iter().min().unwrap();
+    let max_pos = *crab_positions.iter().max().unwrap();
+    let possible_positions = (min_pos..=max_pos).collect::<Vec<_>>();
+
+    find_optimal_fuel_usage(&crab_positions, &possible_positions, fuel_usage_calculator)
+}
+
+fn part1(input: &str) -> usize {
+    run(input, |steps| steps)
+}
+
+fn part2(input: &str) -> usize {
+    run(input, |steps| (0..=steps).sum())
+}
+fn main() {
+    dbg!(part1(INPUT));
+    dbg!(part2(INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+    #[test]
+    fn test_part1_sample() {
+        assert_eq!(part1(SAMPLE_INPUT), 37);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 364898);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 168);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 104149091);
+    }
+}
