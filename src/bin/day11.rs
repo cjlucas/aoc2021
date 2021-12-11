15cacffb2d38f023c7933c8acb66bb5d8f2056c4
Chris Lucas <chris@cjl.io>
2021-12-11 00:49:45 -0500

    part2 done

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -160,7 +160,20 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    todo!()
+    let mut grid: OctopusGrid = input.parse().unwrap();
+
+    for step in 0.. {
+        dbg!(step);
+        println!("{}", grid);
+
+        grid.step();
+
+        if grid.reset_flashes() == 100 {
+            return step + 1;
+        }
+    }
+
+    unreachable!()
 }
 
 fn main() {
@@ -184,13 +197,13 @@ mod tests {
         assert_eq!(part1(INPUT), 6113);
     }
 
-    // #[test]
-    // fn test_part2_sample() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 12);
-    // }
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 195);
+    }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(INPUT), 20373);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 312);
+    }
 }
