8027857554a26efb2b9090e9f41ad74a44cdf1e4
Chris Lucas <chris@cjl.io>
2021-12-14 11:19:19 -0500

    part1 works w/ refactor

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -5,47 +5,6 @@ const INPUT: &'static str = include_str!("../../inputs/day14.txt");
 fn part1(input: &str) -> usize {
     let (template, rules) = input.split_once("\n\n").unwrap();
 
-    let mut template = template.to_string();
-    let rules: HashMap<[char; 2], char> = rules
-        .lines()
-        .map(|line| {
-            let (from, to) = line.split_once(" -> ").unwrap();
-
-            let from = from.chars().take(2).collect::<Vec<_>>().try_into().unwrap();
-            let to = to.chars().nth(0).unwrap();
-
-            (from, to)
-        })
-        .collect();
-
-    for step in 0..10 {
-        let mut insertions: VecDeque<(usize, char)> = VecDeque::new();
-
-        for (idx, window) in template.chars().collect::<Vec<_>>().windows(2).enumerate() {
-            let window: [char; 2] = window.try_into().unwrap();
-
-            if let Some(ch) = rules.get(&window) {
-                insertions.push_front((idx + 1, *ch));
-            }
-        }
-
-        for (idx, ch) in insertions {
-            template.insert(idx, ch);
-        }
-
-        println!("{}: {}", step, template);
-    }
-
-    let counts: Vec<usize> = template.chars().counts().values().cloned().collect();
-    let max = *counts.iter().max().unwrap();
-    let min = *counts.iter().min().unwrap();
-
-    max - min
-}
-
-fn part2(input: &str) -> usize {
-    let (template, rules) = input.split_once("\n\n").unwrap();
-
     let template: Vec<_> = template.chars().collect();
     let mut window_freq: HashMap<[char; 2], usize> = template
         .windows(2)
@@ -63,7 +22,13 @@ fn part2(input: &str) -> usize {
         })
         .collect();
 
-    for step in 0..1 {
+    let mut char_freq: HashMap<char, usize> = template.into_iter().counts();
+
+    // for (k, v) in &mut char_freq {
+    //     *v += 1;
+    // }
+
+    for step in 0..10 {
         let mut insertions: Vec<[char; 2]> = vec![];
         let mut deletions: Vec<[char; 2]> = vec![];
 
@@ -106,13 +71,101 @@ fn part2(input: &str) -> usize {
         }
     }
 
-    // let counts: Vec<usize> = template.chars().counts().values().cloned().collect();
-    // let max = *counts.iter().max().unwrap();
-    // let min = *counts.iter().min().unwrap();
+    for (window, freq) in window_freq {
+        for ch in window {
+            if let Some(ch_freq) = char_freq.get_mut(&ch) {
+                *ch_freq += freq;
+            } else {
+                char_freq.insert(ch, freq);
+            }
+        }
+    }
 
-    // max - min
+    let max = *char_freq.values().max().unwrap();
+    let min = *char_freq.values().min().unwrap();
 
-    0
+    (max - min) / 2
+}
+
+fn part2(input: &str) -> usize {
+    let (template, rules) = input.split_once("\n\n").unwrap();
+
+    let template: Vec<_> = template.chars().collect();
+    let mut window_freq: HashMap<[char; 2], usize> = template
+        .windows(2)
+        .map(|window| window.try_into().unwrap())
+        .counts();
+    let rules: HashMap<[char; 2], char> = rules
+        .lines()
+        .map(|line| {
+            let (from, to) = line.split_once(" -> ").unwrap();
+
+            let from = from.chars().take(2).collect::<Vec<_>>().try_into().unwrap();
+            let to = to.chars().nth(0).unwrap();
+
+            (from, to)
+        })
+        .collect();
+
+    let mut char_freq: HashMap<char, usize> = template.into_iter().counts();
+
+    // for (k, v) in &mut char_freq {
+    //     *v += 1;
+    // }
+
+    for step in 0..40 {
+        let mut insertions: HashMap<[char; 2], usize> = HashMap::new();
+        let mut deletions: HashMap<[char; 2], usize> = HashMap::new();
+
+        for (window, &freq) in window_freq.iter() {
+            if freq == 0 {
+                continue;
+            }
+
+            let window: [char; 2] = (*window).try_into().unwrap();
+
+            if let Some(ch) = rules.get(&window) {
+                let mut first = window.clone();
+                first[1] = *ch;
+                let mut second = window.clone();
+                second[0] = *ch;
+
+                *insertions.entry(first).or_default() += freq;
+                *insertions.entry(second).or_default() += freq;
+
+                *deletions.entry(window).or_default() += freq;
+
+                // insertions.push_front((idx + 1, *ch));
+            }
+        }
+
+        // dbg!(&window_freq.len());
+        // dbg!(&insertions.len());
+        // dbg!(&deletions.len());
+
+        for (window, num_insertions) in insertions {
+            *window_freq.entry(window).or_default() += num_insertions;
+        }
+
+        for (window, num_deletions) in deletions {
+            *window_freq.entry(window).or_default() += num_deletions;
+        }
+    }
+
+    for (window, freq) in window_freq {
+        for ch in window {
+            if let Some(ch_freq) = char_freq.get_mut(&ch) {
+                *ch_freq += freq;
+            } else {
+                char_freq.insert(ch, freq);
+            }
+        }
+    }
+
+    let max = *char_freq.values().max().unwrap();
+    let min = *char_freq.values().min().unwrap();
+
+    (max - min) / 2
 }
 
 fn main() {
